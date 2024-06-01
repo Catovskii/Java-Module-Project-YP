7c7d8f2bd504342cab463dcaa16b51fba41753ba
Catovskii <demchuk.vasya@gmail.com>
2024-06-01 21:46:34 +0300

    Update Rub.java
    Исправлено. Без комментариев. Проблема не кодирования, а знания русского языка))

--- src/main/java/calc/Rub.java
+++ src/main/java/calc/Rub.java
@@ -1,11 +1,14 @@
 package calc;
 
 class Rub {
+
     static String returnRubLine(double payment){
         String rub;
-        if (payment > 10 && payment <20)  { rub = "рублей"; } // Добавлен вариант с десятками
+
+        if ((int)(Math.floor(payment)) % 100 > 10 && (int)(Math.floor(payment)) % 100 < 15)
+            { rub = "рублей"; }
             else {
-            rub = switch ((int)(Math.floor(payment))%10)    // Сумма может быть и 123.04 Нас интересует только цифра 3.
+            rub = switch ((int)(Math.floor(payment))%10)    
             {   
                 case 1 -> "рубль";
                 case 2, 3, 4 -> "рубля";
